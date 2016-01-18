42358df9c690f1e3ff1d78d78cc2eac7a6e28c39
David Slone <dbslone@gmail.com>
2016-01-18 10:04:33 -0800

    create actions to retrieve postgres information

--- app/reducers/postgres.js
+++ app/reducers/postgres.js
@@ -1,13 +1,17 @@
 import {
   POSTGRES_ADD_TABLES,
+  POSTGRES_ADD_CONNECTION,
+  POSTGRES_PERFORMANCE,
   POSTGRES_QUERY_DATA
 } from '../actions/action_types'
 
 import {groupBy} from 'lodash'
 
 let initialState = {
+  connection: {},
   databases: [],
-  queryData: {}
+  queryData: {},
+  performance: []
 }
 
 export default function postgres (state = initialState, action) {
@@ -15,15 +19,32 @@ export default function postgres (state = initialState, action) {
   switch (action.type) {
     case POSTGRES_ADD_TABLES: {
       let databases = groupBy(action.payload, (n) => n.table_catalog)
-
+console.log('databases: ', databases)
       return {
         ...state,
         databases
       }
     }
 
+    case POSTGRES_ADD_CONNECTION: {
+      return {
+        ...state,
+        connection: action.payload
+      }
+    }
+
     case POSTGRES_QUERY_DATA: {
-      
+      return {
+        ...state,
+        queryData: action.payload
+      }
+    }
+
+    case POSTGRES_PERFORMANCE: {
+      return {
+        ...state,
+        performance: action.payload
+      }
     }
 
     default:
