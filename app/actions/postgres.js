a54e3eca87e6617d4f5b12c1091e0849936efef9
David Slone <dbslone@gmail.com>
2016-01-18 10:08:21 -0800

    create actions to store postgres information

--- app/actions/postgres.js
+++ app/actions/postgres.js
@@ -1,7 +1,21 @@
 import {
+  POSTGRES_ADD_CONNECTION,
   POSTGRES_ADD_TABLES,
+  POSTGRES_PERFORMANCE,
   POSTGRES_QUERY_DATA
 } from './action_types'
+
+// import {postgres} from '../queries'
+
+let massive = require('massive')
+
+export function postgresAddConnection (payload) {
+
+  return {
+    type: POSTGRES_ADD_CONNECTION,
+    payload
+  }
+}
 
 export function postgresAddTables (payload) {
 
@@ -19,16 +33,52 @@ export function updatePostgresQueryData (payload) {
   }
 }
 
-export function postgresExecuteQuery (payload) {
+export function postgresUpdatePerformance (payload) {
+
+  return {
+    type: POSTGRES_PERFORMANCE,
+    payload
+  }
+}
+
+export function postgresListTables () {
 
   return (dispatch, getState) => {
 
-    let connectionString = 'postgres://dbslone@localhost:15432/decoraid_development'
-    let connection = massive.connectSync({connectionString})
+    let state = getState()
+    let connection = massive.connectSync({connectionString: state.postgres.connection.connectionString})
 
-    connection.run(listTablesQuery, (err, res) => {
-      this.props.addTables(res)
+    connection.run("SELECT * FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema = 'public' ORDER BY table_catalog, table_name", (err, res) => {
+
+      dispatch(postgresAddTables(res))
     })
-    dispatch(updatePostgresQueryData())
+  }
+}
+
+export function postgresExecuteQuery (query) {
+
+  return (dispatch, getState) => {
+
+    let state = getState()
+    let connection = massive.connectSync({connectionString: state.postgres.connection.connectionString})
+
+    connection.run(query, (err, response) => {
+
+      dispatch(updatePostgresQueryData(response))
+    })
+  }
+}
+
+export function postgresPerformanceQuery () {
+
+  return (dispatch, getState) => {
+
+    let state = getState()
+    let connection = massive.connectSync({connectionString: state.postgres.connection.connectionString})
+
+    connection.run('SELECT * FROM pg_stat_activity', (err, response) => {
+
+      // dispatch(postgresUpdatePerformance(response))
+    })
   }
 }
