02962a4541fc826bf32f5c3cb55ac1026734c553
David Slone <dbslone@gmail.com>
2016-05-17 22:24:01 -0700

    WIP

--- app/packages/postgres_view/performance.js
+++ app/packages/postgres_view/performance.js
@@ -1,14 +1,6 @@
 import React, {Component, PropTypes} from 'react'
 import {map, padStart} from 'lodash'
 
-import Table from 'material-ui/lib/table/table'
-import TableBody from 'material-ui/lib/table/table-body'
-import TableFooter from 'material-ui/lib/table/table-footer'
-import TableHeader from 'material-ui/lib/table/table-header'
-import TableHeaderColumn from 'material-ui/lib/table/table-header-column'
-import TableRow from 'material-ui/lib/table/table-row'
-import TableRowColumn from 'material-ui/lib/table/table-row-column'
-
 class Performance extends Component {
 
   static propTypes = {
@@ -25,55 +17,35 @@ class Performance extends Component {
     this.props.getPerformanceResults()
   }
 
-  renderResults () {
-
-    let results = map(this.props.performance, (row) => {
-
-      return (
-        <TableRow selected={true}>
-          <TableRowColumn>{row.datname}</TableRowColumn>
-          <TableRowColumn>{row.query}</TableRowColumn>
-          <TableRowColumn>{padStart(row.duration.minutes, 2)}:{padStart(row.duration.seconds, 2)}</TableRowColumn>
-        </TableRow>
-      )
-    })
-
-    return results
-  }
+  // renderResults () {
+  //
+  //   let results = map(this.props.performance, (row) => {
+  //
+  //     return (
+  //       <TableRow selected={true}>
+  //         <TableRowColumn>{row.datname}</TableRowColumn>
+  //         <TableRowColumn>{row.query}</TableRowColumn>
+  //         <TableRowColumn>{padStart(row.duration.minutes, 2, '0')}:{padStart(row.duration.seconds, 2)}</TableRowColumn>
+  //       </TableRow>
+  //     )
+  //   })
+  //
+  //   return results
+  // }
 
   render () {
 
-    let tableprops = {
-      fixedHeader: true,
-      fixedFooter: true,
-      stripedRows: false,
-      showRowHover: false,
-      selectable: true,
-      multiSelectable: false,
-      enableSelectAll: false,
-      deselectOnClickaway: true,
-      height: '300px'
-    }
+    let dataList = [
+      ['col 1', 'col2', 'col3'],
+      ['col 1', 'col2', 'col3']
+    ]
 
     return (
-      <Table
-        fixedHeader={tableprops.fixedHeader}
-        fixedFooter={tableprops.fixedFooter}
-        onRowSelection={this._onRowSelection}>
-        <TableBody
-          displayRowCheckbox={false}
-          deselectOnClickaway={tableprops.deselectOnClickaway}
-          showRowHover={tableprops.showRowHover}
-        >
-          {this.renderResults()}
-        </TableBody>
-        <TableFooter>
-          <TableRow>Show More</TableRow>
-        </TableFooter>
-      </Table>
+      <div>
+        Performance
+      </div>
     )
   }
-
 }
 
 export default Performance
--- app/packages/postgres_view/table_view.js
+++ app/packages/postgres_view/table_view.js
@@ -1,5 +1,11 @@
 import React, {Component, PropTypes} from 'react'
-import {Table, Column, Cell} from '../flex_table'
+import {Table, Column, Cell} from 'fixed-data-table'
+
+const TextCell = ({rowIndex, data, columnKey, ...props}) => (
+  <Cell {...props}>
+    {data.getObjectAt(rowIndex)[columnKey]}
+  </Cell>
+)
 
 class TableView extends Component {
 
@@ -31,18 +37,52 @@ class TableView extends Component {
       color: 'black'
     }
 
+    const dataList = ['test1','test2']
+
     return (
       <div style={styles} className="tableView">
-        <Table>
-          <Column>
-            <Cell>R1 C1</Cell>
-            <Cell>R2 C2</Cell>
-          </Column>
-          <Column>
-            <Cell>R1 C1</Cell>
-            <Cell>R2 C2</Cell>
-          </Column>
-        </Table>
+      <Table
+        rowHeight={30}
+        headerHeight={50}
+        rowsCount={5}
+        onColumnResizeEndCallback={() => {}}
+        isColumnResizing={false}
+        width={1000}
+        height={500}
+        {...this.props}>
+        <Column
+          columnKey="firstName"
+          header={<Cell>First Name</Cell>}
+          cell={<Cell>Column 1</Cell>}
+          fixed={true}
+          width={100}
+          isResizable={true}
+        />
+        <Column
+          columnKey="lastName"
+          header={<Cell>Last Name (min/max constrained)</Cell>}
+          cell={<Cell>Column 1</Cell>}
+          width={100}
+          isResizable={true}
+          minWidth={70}
+          maxWidth={170}
+        />
+        <Column
+          columnKey="companyName"
+          header={<Cell>Company</Cell>}
+          cell={<Cell>Column 1</Cell>}
+          width={100}
+          isResizable={true}
+        />
+        <Column
+          columnKey="sentence"
+          header={<Cell>Sentence</Cell>}
+          cell={<Cell>Column 1</Cell>}
+          width={100}
+          isResizable={true}
+        />
+      </Table>
+
       </div>
     )
   }
